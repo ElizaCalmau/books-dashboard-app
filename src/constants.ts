8cb78ef07091ba60215e36bf4a677ff69a41995f
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-06 00:07:21 +0200

    created TABLE_HEADERS enum

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export enum TABLE_HEADERS {
+    TITLE = "Title",
+    AUTHOR="Author",
+    CATEGORY="Category",
+    ISBN="ISBN",
+    CREATED_AT="Created At",
+    MODIFIED_AT="Modified At",
+    ACTIVE="Active",
+}
