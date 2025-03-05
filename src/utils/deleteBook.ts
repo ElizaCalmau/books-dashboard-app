89265f0c601bfc19087a71c424efca032693512d
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-06 00:07:48 +0200

    created utility deleteBook

---
+++ src/utils/deleteBook.ts
@@ -0,0 +1,19 @@
+export const deleteBook = async (id: string) => {
+    await fetch(`http://localhost:3000/books/${id}`, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to delete book');
+        }
+        return response.json();
+    })
+        .then(updatedBook => {
+            console.log('Deleted Book:', updatedBook);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });//TODO: tooltip with message 'you've deleted a book title', page reload after delete
+}
