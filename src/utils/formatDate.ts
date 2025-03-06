ed1a0c955f4853756b12740579a7849a904af200
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-06 22:26:54 +0200

    created formatDate utility

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,10 @@
+export const formatDate = (date: string | Date) => {
+    return new Date(date).toLocaleString('en-GB', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true,
+    });
+};
