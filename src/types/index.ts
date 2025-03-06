c13983c3468080255e4ba78261bca88e84ba92ac
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-06 17:50:15 +0200

    add Category union type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,9 +2,11 @@ export interface Book {
     id: string;
     title: string,
     author: string,
-    category: string,
+    category: Category,
     isbn: number,
     createdAt: string,
     modifiedAt: string,
     active: boolean,
 }
+
+export type Category = 'fiction' | 'finance' | 'science' | 'thriller';
