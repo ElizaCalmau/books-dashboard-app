81a9b308b3c34825ed3ef33f85ad3d41dccfaa9c
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-06 16:59:35 +0200

    added routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from "react-router";
+import {EditPage} from "./components/EditPage.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+        <StrictMode>
+            <Routes>
+                <Route path='/' element={<App />}/>
+
+                <Route path=':id' element={<EditPage />} />
+                <Route path="*" element={<h1> Sorry, page Not found</h1>}/>
+            </Routes>
+
+        </StrictMode>
+    </BrowserRouter>,
+
 )
