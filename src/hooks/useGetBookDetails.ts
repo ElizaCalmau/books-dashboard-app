bc38eca5569f36935f42bd89b815e453055bb826
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-06 16:58:21 +0200

    created custom hook useGetBookDetails

---
+++ src/hooks/useGetBookDetails.ts
@@ -0,0 +1,29 @@
+import {useEffect, useState} from "react";
+import {Book} from "../types";
+import {getBookDetails} from "../utils/getBookDetails.ts";
+
+export const useGetBookDetails = (id: string) => {
+    const [bookDetails, setBookDetails] = useState<Book>(
+        {
+            title: '',
+            author: '',
+            id: '',
+            category: '',
+            createdAt: '',
+            modifiedAt: '',
+            active: true,
+            isbn: 0}
+    );
+
+    useEffect(() => {
+            const fetchData = async () => {
+                const data = await getBookDetails({endpoint: 'books', param: id});
+                console.log(data)
+                setBookDetails(data);
+            };
+            fetchData();
+
+
+    }, [id]);
+    return {bookDetails, setBookDetails};
+};
