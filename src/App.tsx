ee16baf03c9110973c5434a1e8e167b2aa1ec967
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-06 22:27:44 +0200

    created 'add book' button with generated id

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {Table} from "./components/Table.tsx";
-
+import {ButtonLink} from "./components/ButtonLink.tsx";
+import { v4 as uuidv4 } from 'uuid';
 function App() {
-
+    const id = uuidv4();
   return (
     <>
       <Table/>
+        <ButtonLink text='Add Book' id={id}/>
     </>
   )
 }
