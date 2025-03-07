61b6a7d2fc8db6d90efb219ca9fe5c264ecc426f
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-07 11:32:57 +0200

    created BookContext

---
+++ src/context/BookContext.tsx
@@ -0,0 +1,22 @@
+import {createContext, ReactNode, useContext, useState} from "react";
+import {Book, BookContextType, initialBook} from "../constants.ts";
+
+export const BookContext = createContext<BookContextType | undefined>(undefined);
+
+export const BookProvider = ({ children }: { children: ReactNode }) => {
+    const [isNewBook, setIsNewBook] = useState<boolean>(false);
+    const [bookDetails, setBookDetails] = useState<Book>(initialBook)
+    const [error, setError] = useState('');
+    return (
+        <BookContext.Provider value={{ isNewBook, setIsNewBook, bookDetails, setBookDetails, error, setError }}>
+            {children}
+        </BookContext.Provider>
+    );
+};
+export const useBookContext = () => {
+    const context = useContext(BookContext);
+    if (!context) {
+        throw new Error("useBookContext must be used within a BookProvider");
+    }
+    return context;
+};
