b918faab4adb868f29f9f5c72ac95220d1d41f5d
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-07 11:33:14 +0200

    wrapped Routes in BookProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,25 @@
-import { StrictMode } from 'react'
+import {StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from "react-router";
 import {EditPage} from "./components/EditPage.tsx";
+import {BookProvider} from "./context/BookContext.tsx";
+
+
+
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <StrictMode>
-            <Routes>
-                <Route path='/' element={<App />}/>
+            <BookProvider>
+                <Routes>
+                    <Route path='/' element={<App />}/>
+                    <Route path=':id' element={<EditPage />} />
+                    <Route path="*" element={<h1> Sorry, page Not found</h1>}/>
+                </Routes>
+            </BookProvider>
 
-                <Route path=':id' element={<EditPage />} />
-                <Route path="*" element={<h1> Sorry, page Not found</h1>}/>
-            </Routes>
 
         </StrictMode>
     </BrowserRouter>,
