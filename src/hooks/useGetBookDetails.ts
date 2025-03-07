dc53da2f1c2f73200aca06e4f636418820b3c9a4
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-07 11:44:25 +0200

    using a values from context to update a context

--- src/hooks/useGetBookDetails.ts
+++ src/hooks/useGetBookDetails.ts
@@ -1,32 +1,26 @@
 import {useEffect, useState} from "react";
 import {Book} from "../types";
 import {getBookDetails} from "../utils/getBookDetails.ts";
+import {useEffect} from "react";
+
+import {getBookById} from "../utils/bookService.ts";
+import {useBookContext} from "../context/BookContext.tsx";
+import {initialBook} from "../constants.ts";
 
 export const useGetBookDetails = (id: string) => {
-    const [bookDetails, setBookDetails] = useState<Book>(
-        {
-            title: '',
-            author: '',
-            id: '',
-            category: 'fiction',
-            isbn: 0,
-            createdAt: '',
-            modifiedAt: '',
-            active: true,
-        }
-    );
-    const [isNewBook, setIsNewBook] = useState<boolean>(false);
+    const {setIsNewBook, setBookDetails} = useBookContext()
     useEffect(() => {
 
             const fetchData = async () => {
                 try {
                     const data = await getBookDetails({ endpoint: 'books', param: id });
 
+                    const data = await getBookById(id);
+                    setBookDetails(data);
+                    setIsNewBook(false);
                     if (!data || Object.keys(data).length === 0) {
                         throw new Error('Book not found');
                     }
-                    setBookDetails(data);
-                    setIsNewBook(false);
                 } catch (error) {
                     console.log(error);
                     setIsNewBook(true);
@@ -38,8 +32,5 @@ export const useGetBookDetails = (id: string) => {
                 }
             };
             fetchData();
-
-
     }, [id]);
-    return {bookDetails, setBookDetails, isNewBook, setIsNewBook};
 };
