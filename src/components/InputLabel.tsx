460fc22d6a64b58d1a80b0af93dc8400e9b8e2ee
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-10 23:23:36 +0200

    refactored InputLabel and type annotations

--- src/components/InputLabel.tsx
+++ src/components/InputLabel.tsx
@@ -1,30 +1,25 @@
-import React, {ChangeEvent, ChangeEventHandler} from "react";
+import {CategoryOption, InputField} from "../constants.ts";
+import React from "react";
 
-interface Props {
-    name: string;
-    label: string;
-    placeholder?: string | undefined;
-    value: string | number;
-    onChange: (value: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => void;
-    validator?: (value: string | number) => boolean;
-    type?: 'string' | 'number';
-    options?: {label: string, value: string}[];
+interface Prop {
+    field: InputField,
+    onChange?: (value: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => void,
 }
 
-export const InputLabel: React.FC<Props> = ({name, label, placeholder, value, onChange, type, options}) => {
-
+export const InputLabel: React.FC<Prop> = ({field, onChange}) => {
+    const {value, label, placeholder, name, options, type, required} = field;
     return (
         <><label htmlFor={name}> {label}</label>
             {options ? (
                     <select id={name} name={name} value={value} onChange={onChange}>
-                        {options.map((option) => (
+                        {options.map((option: CategoryOption) => (
                             <option key={option.value} value={option.value}>
                                 {option.label}
                             </option>
                         ))}
                     </select>
                 ) :
-            (<input required type={type} name={name} id={name} placeholder={placeholder} value={value} onChange={onChange} />) }
+            (<input required={required} type={type} name={name} id={name} placeholder={placeholder} value={value} onChange={onChange} />) }
                 </>
     );
 };
