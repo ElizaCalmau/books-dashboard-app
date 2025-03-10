51cb3a875f96ef61eb95cf18f9cf17a078a8220e
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-10 21:50:59 +0200

    updated import for Table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import {Table} from "./components/Table.tsx";
+
 import {ButtonLink} from "./components/ButtonLink.tsx";
 import { v4 as uuidv4 } from 'uuid';
+import {Table} from "./components/Table/Table.tsx";
 function App() {
     const id = uuidv4();
   return (
