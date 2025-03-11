e88b2267a77a73b0cca373b4ddb3e1e8a613c1c8
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-11 15:02:57 +0200

    used Select component inside InputLabel component

--- src/components/InputLabel.tsx
+++ src/components/InputLabel.tsx
@@ -1,5 +1,6 @@
-import {CategoryOption, InputField} from "../constants.ts";
+import {InputField} from "../constants.ts";
 import React from "react";
+import {Select} from "./Select.tsx";
 
 interface Prop {
     field: InputField,
@@ -10,16 +11,11 @@ export const InputLabel: React.FC<Prop> = ({field, onChange}) => {
     const {value, label, placeholder, name, options, type, required} = field;
     return (
         <><label htmlFor={name}> {label}</label>
-            {options ? (
-                    <select id={name} name={name} value={value} onChange={onChange}>
-                        {options.map((option: CategoryOption) => (
-                            <option key={option.value} value={option.value}>
-                                {option.label}
-                            </option>
-                        ))}
-                    </select>
-                ) :
-            (<input required={required} type={type} name={name} id={name} placeholder={placeholder} value={value} onChange={onChange} />) }
-                </>
+            {options ?
+                <Select value={value} name={name} onChange={onChange} type={type} required={required} options={options}/>
+                :
+                (<input required={required} type={type} name={name} id={name} placeholder={placeholder} value={value} onChange={onChange} />)
+            }
+        </>
     );
 };
