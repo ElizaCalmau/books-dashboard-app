4084b0f20ed6755e301a9e633b737356cdaeb487
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-11 15:14:05 +0200

    leveraged filtered useFilterBooks and useBooksList custom hooks to filter the books

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@
 import {ButtonLink} from "./components/ButtonLink.tsx";
 import { v4 as uuidv4 } from 'uuid';
 import {Table} from "./components/Table/Table.tsx";
+import {useBooksListContext} from "./context/BooksListContext.tsx";
+import {filterOptions} from "./constants.ts";
+import {Select} from "./components/Select.tsx";
+import {handleFilterChange} from "./utils/handleFilterChange.ts";
+import {useFilterBooks} from "./hooks/useFilterBooks.ts";
+
 function App() {
+    const { filter, setFilter } = useBooksListContext();
+    const { filteredBooks } = useFilterBooks();
+
     const id = uuidv4();
-  return (
-    <>
-      <Table/>
-        <ButtonLink text='Add Book' id={id}/>
-    </>
+    return (
+        <>
+            <Select options={filterOptions} name='filter' value={filter.value} onChange={(e) => handleFilterChange(e, setFilter)} type='select' required={true} />
+            <Table books={filteredBooks}/>
+            <ButtonLink text='Add Book' id={id}/>
+        </>
   )
 }
 
