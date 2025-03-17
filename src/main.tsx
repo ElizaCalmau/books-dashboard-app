a10450b15fff65a6085712d03e029e7b1bee2de4
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-17 16:52:02 +0200

    added BooksProvider to the router

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from "react-router";
 import {EditPage} from "./components/EditPage.tsx";
 import {BookProvider} from "./context/BookContext.tsx";
-import {BooksListProvider} from "./context/BooksListContext.tsx";
 import {Header} from "./components/Header/Header.tsx";
+import {BooksProvider} from "./context/BooksContext.tsx";
 
 
 
@@ -14,18 +14,16 @@ import {Header} from "./components/Header/Header.tsx";
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <StrictMode>
-            <BooksListProvider>
                 <BookProvider>
-                    <Header />
-                    <Routes>
-
-                        <Route path='/' element={<App />}/>
-                        <Route path=':id' element={<EditPage />} />
-                        <Route path="*" element={<h1> Sorry, page Not found</h1>}/>
-                    </Routes>
+                    <BooksProvider>
+                            <Header />
+                            <Routes>
+                                <Route path='/' element={<App />}/>
+                                <Route path=':id' element={<EditPage />} />
+                                <Route path="*" element={<h1> Sorry, page Not found</h1>}/>
+                            </Routes>
+                    </BooksProvider>
                 </BookProvider>
-            </BooksListProvider>
-
         </StrictMode>
     </BrowserRouter>,
 
