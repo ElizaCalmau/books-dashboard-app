f5d6646f041ddd16cf019f471740ab384f2ea4f3
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-18 13:39:33 +0200

    added styles to table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -19,40 +19,42 @@ export const Table = ({books} : {books: Book[]}) => {
     if(books.length > 0){
         return (
             <>
-
                 <table className={styles.table}>
-                    <thead className={styles.head}>
-                    <tr>
+                    <thead>
+                    <tr className={styles.tableHeadRow}>
                         {Object.values(TABLE_HEADERS).slice(0, -1).map((header) => (
-                            <th key={header}>{header}</th>
+                            <th key={header} className={styles.tableHeadItem}>{header}</th>
                         ))}
-                        <th>Actions</th>
+                        <th className={styles.tableHeadItem}>Actions</th>
                     </tr>
                     </thead>
                     <tbody>
                     {books.length > 0 && books.map((book: Book) => {
-                        const {id, ...rest} = book;
-                        return ( <tr key={id}>
+                        const {id, active, ...rest} = book;
+                        return <tr key={id} className={styles.tableRow}>
                             {Object.values(rest).map((prop) => {
                                 const itemId = uuidv4()
-                                    return(<td key={itemId}>
+                                console.log(prop)
+                                    return <td key={itemId} className={styles.tableDataItem}>
                                             {prop}
-                                        </td>)
+                                        </td>
 
                             })}
-                            <td>
+
+                            <td className={styles.tableDataItem}>
                                 <ButtonLink text='Edit' id={id}/>
-                            </td>
-                            <td>
+
+
                                 <Button text='Delete' onClick={() => deleteBook(id)}/>
-                            </td>
-                            <td>
+
+
                                 <Button text={book.active ? 'Deactivate' : 'Activate'} onClick={()=>handleBookStateUpdate({id, book})}/>
+
+
+                                {/*State: {book.active ? 'activated' : 'deactivated'}*/}
+                                {/*TODO: make a green or red light for book state*/}
                             </td>
-                            <td>
-                                Book state: {book.active ? 'activated' : 'deactivated'}
-                            </td>
-                        </tr>)
+                        </tr>
 
                     })}
                     </tbody>
