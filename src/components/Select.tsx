8dbdc2836c00b0f5f9a005bb515e8060d15a6c6d
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-19 16:42:13 +0200

    added required prop to a Sidebar

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,10 +1,10 @@
 import {CategoryOption, FilterOption, SelectProps} from "../constants.ts";
 import React from "react";
 
-export const Select: React.FC<SelectProps> = ({options, name, value, onChange}) => {
+export const Select: React.FC<SelectProps> = ({options, name, value, onChange, required}) => {
     return (
         <>
-        <select id={name} name={name} value={value} onChange={onChange}>
+        <select id={name} name={name} value={value} onChange={onChange} required={required}>
             {options?.map((option: CategoryOption | FilterOption) => (
                 <option key={option.value} value={option.value}>
                     {option.label}
