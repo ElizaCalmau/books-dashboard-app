bcd1f24d3a0f1885f8ed7d94c883f1fb4bd934e7
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-21 23:26:25 +0200

    fixed onClick argument type annotation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,10 @@
 import React, {JSX} from "react";
 import styles from "./Button.module.scss";
+
 interface Prop {
     text?: string,
     icon?: JSX.Element,
-    onClick: () => void;
+    onClick?: (event: React.MouseEvent) => void
 }
 
 export const Button: React.FC<Prop> = ({text, onClick, icon}) => {
