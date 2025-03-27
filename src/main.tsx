cbc0e57340a8b3865b9c4537d65557aeafea6107
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-27 18:29:33 +0200

    add Burger to common layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from "react-router";
-import {EditPage} from "./components/EditPage.tsx";
+import {EditPage} from "./components/EditPage/EditPage.tsx";
 import {BookProvider} from "./context/BookContext.tsx";
 import {BooksProvider} from "./context/BooksContext.tsx";
 import {Sidebar} from "./components/Sidebar/Sidebar.tsx";
+import {Burger} from "./components/Burger/Burger.tsx";
 
 
 
@@ -16,7 +17,7 @@ createRoot(document.getElementById('root')!).render(
         <StrictMode>
                 <BookProvider>
                     <BooksProvider>
-                            <Sidebar />
+                            <Sidebar /> <Burger/>
                             <Routes>
                                 <Route path='/' element={<App />}/>
                                 <Route path='/update-book/:id' element={<EditPage />} />
