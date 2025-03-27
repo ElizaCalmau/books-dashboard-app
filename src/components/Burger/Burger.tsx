6c889becebbd2907ccae32d81789c857a63c2ace
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-27 17:01:49 +0200

    added burger menu animated icon

---
+++ src/components/Burger/Burger.tsx
@@ -0,0 +1,19 @@
+import styles from './Burger.module.scss'
+import {useState} from "react";
+import classNames from "classnames";
+
+export const Burger = () => {
+    const [toggle, setToggle] = useState<boolean>(false)
+    const barClassNames = classNames(styles.bar, {[styles.cross]: !toggle})
+
+    return (
+        <div className={styles.burgerWrapper} onClick={() => setToggle(!toggle)}>
+            <div className={barClassNames}>
+            </div>
+            <div className={barClassNames}>
+            </div>
+            <div className={barClassNames}>
+            </div>
+        </div>
+    );
+};
