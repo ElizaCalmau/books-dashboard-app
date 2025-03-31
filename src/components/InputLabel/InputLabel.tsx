01b19f0ced838add90effb344dbe8053834407b2
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-03-31 13:56:58 +0300

    put inputLabel into a separate folder

---
+++ src/components/InputLabel/InputLabel.tsx
@@ -0,0 +1,23 @@
+import {InputField} from "../../constants.ts";
+import React from "react";
+import {Select} from "../Select/Select.tsx";
+import styles from './InputLabel.module.scss'
+
+interface Prop {
+    field: InputField,
+    onChange?: (value: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => void,
+}
+
+export const InputLabel: React.FC<Prop> = ({field, onChange}) => {
+    const {value, label, placeholder, name, options, type, required} = field;
+    return (
+        <>
+            <label className={styles.label} htmlFor={name}> {label}</label>
+            {options ?
+                <Select value={value} type={type} required={required} options={options} onClick={onChange}/>
+                :
+                <input className={styles.customInput} required={required} type={type} name={name} id={name} placeholder={placeholder} value={value} onChange={onChange} />
+            }
+        </>
+    );
+};
