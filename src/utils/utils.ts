f481e28f577f060095b082d3dfafa91fc77b16ca
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-02 20:34:16 +0300

    use Prevent Default to avoid event bubbling to submit button

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 import React from "react";
 
 export const goBack = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.stopPropagation();
+        e.preventDefault();
         window.history.back()
 }
