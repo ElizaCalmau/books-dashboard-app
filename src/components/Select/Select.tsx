63ba27e19ca2efe88abafb5553c8bd41cf4d09fe
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-03 15:07:08 +0300

    set both toLowerCase to keep consistency across all the use cases

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -15,7 +15,7 @@ export const Select: React.FC<SelectProps> = ({options, value, onClick}) => {
         setIsOpen(!isOpen);
     }
 
-    const option = options?.find(option => option.label.toLowerCase() == value);
+    const option = options?.find(option => option.label.toLowerCase() == value.toLowerCase());
     return (
         <div className={selectClassName} onClick={toggleOpen}>
             <div className={styles.selectedOption}>
