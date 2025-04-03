8f8c267429a62ca08de99c1cc7863fcb1c20f08f
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-03 17:29:19 +0300

    added AboutPage to Routes

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import {BookProvider} from "./context/BookContext.tsx";
 import {BooksProvider} from "./context/BooksContext.tsx";
 import {Sidebar} from "./components/Sidebar/Sidebar.tsx";
 import {Burger} from "./components/Burger/Burger.tsx";
+import {AboutPage} from "./components/AboutPage/AboutPage.tsx";
 
 
 
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')!).render(
                             <Routes>
                                 <Route path='/' element={<App />}/>
                                 <Route path='/update-book/:id' element={<EditPage />} />
+                                <Route path='/about' element={<AboutPage />} />
                                 <Route path="*" element={<h1> Sorry, page Not found</h1>}/>
                             </Routes>
                     </BooksProvider>
