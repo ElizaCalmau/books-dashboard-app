532d39feb4132485b056175dacc4c581df3e65a1
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-03 18:22:35 +0300

    added About to sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import {BookIcon, BookmarkPlusIcon} from "lucide-react";
+import {BookIcon, BookmarkPlusIcon, InfoIcon} from "lucide-react";
 import {v4 as uuidv4} from "uuid";
 import styles from "./Sidebar.module.scss"
 import {ButtonLink} from "../ButtonLink/ButtonLink.tsx";
@@ -9,6 +9,7 @@ export const Sidebar = () => {
             <div className={styles.buttonContainer}>
                 <ButtonLink path='/' icon={<BookIcon size={30}/>} text="Home"/>
                 <ButtonLink path={`/update-book/${id}`} icon={<BookmarkPlusIcon size={30}/>} text="Add Book"/>
+                <ButtonLink path='/about' icon={<InfoIcon size={30}/>} text="About" />
             </div>
         </div>
     );
