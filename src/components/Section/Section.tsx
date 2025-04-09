df38def9610436001bd01553485d78f1738ab7c1
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-09 18:02:00 +0300

    added styles for Section

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -2,20 +2,24 @@ import styles from "./Section.module.scss";
 
 import React, {useState} from "react";
 import {TwistingArrow} from "../TwistingArrow/TwistingArrow.tsx";
+import classNames from "classnames";
 
 interface SectionProps {
     title: string;
+    subtitle?: string;
     paragraph: string;
 }
-export const Section: React.FC<SectionProps> = ({title, paragraph}) => {
+export const Section: React.FC<SectionProps> = ({title, paragraph, subtitle}) => {
     const [isOpen, setIsOpen] = useState(false);
+    const sectionClassNames = classNames({[styles.sectionVisible]: isOpen}, {[styles.sectionHidden]: !isOpen});
     return (
         <section className={styles.sectionWrapper}>
             <div className={styles.headingWrapper} onClick={() => setIsOpen(!isOpen)}>
                 <h2>{title}</h2>
                 <TwistingArrow isOpen={isOpen}/>
             </div>
-            <p className={isOpen ? styles.open : styles.close}>{paragraph}</p>
+            <div>{subtitle}</div>
+            <p className={sectionClassNames}>{paragraph}</p>
         </section>
     );
 };
