39bde08239a60d5e6711ac65af61857c75a650fd
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-11 18:19:08 +0300

    added contacts page to Routes

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import {BooksProvider} from "./context/BooksContext.tsx";
 import {Sidebar} from "./components/Sidebar/Sidebar.tsx";
 import {Burger} from "./components/Burger/Burger.tsx";
 import {AboutPage} from "./components/AboutPage/AboutPage.tsx";
+import {ContactsPage} from "./components/ContactsPage/ContactsPage.tsx";
 
 
 
@@ -18,11 +19,13 @@ createRoot(document.getElementById('root')!).render(
         <StrictMode>
                 <BookProvider>
                     <BooksProvider>
-                            <Sidebar /> <Burger/>
+                            <Sidebar />
+                            <Burger/>
                             <Routes>
                                 <Route path='/' element={<App />}/>
                                 <Route path='/update-book/:id' element={<EditPage />} />
                                 <Route path='/about' element={<AboutPage />} />
+                                <Route path='/contacts' element={<ContactsPage />} />
                                 <Route path="*" element={<h1> Sorry, page Not found</h1>}/>
                             </Routes>
                     </BooksProvider>
