9ddec24841a19fcc79705df0dba8292b58a43c82
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-17 16:58:49 +0300

    fixed type annotation for getAllBooks result

--- src/hooks/useGetBooks.ts
+++ src/hooks/useGetBooks.ts
@@ -7,8 +7,8 @@ export function useGetBooks() {
 
     const fetchData = useCallback(async () => {//function memoization prevents from calling on each render, only once if dependency array is empty and on-demand if there's some changes in deps array
         try {
-            const res: Book[] = await getAllBooks();
-            setBooksList(res);
+            const res: Book[] | undefined = await getAllBooks();
+            setBooksList(res || []);
         } catch (error: unknown) {
             throw new Error('error getBooks');
         }
