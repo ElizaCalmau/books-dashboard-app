d14e38d67d4843a649a0203aaf25f8b9ba245d94
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-17 16:30:44 +0300

    added margin for Loading container

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,13 +1,17 @@
 import {Blocks} from "react-loader-spinner";
 
 export const Loading = () => {
-    return <Blocks
-        height="80"
-        width="80"
-        color="#4fa94d"
-        ariaLabel="blocks-loading"
-        wrapperStyle={{}}
-        wrapperClass="blocks-wrapper"
-        visible={true}
-    />
+    return (
+        <div style={{marginTop: '3em'}}>
+            <Blocks
+                height="80"
+                width="80"
+                color="#4fa94d"
+                ariaLabel="blocks-loading"
+                wrapperStyle={{}}
+                wrapperClass="blocks-wrapper"
+                visible={true}
+            />
+        </div>
+        )
 }
