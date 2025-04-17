ce2283fc9248eb89f4bd1e0ef038f20a3b47004a
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-17 16:50:21 +0300

    created serviceHandler reusable function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,3 +4,19 @@ export const goBack = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         window.history.back()
 }
+
+export const serviceHandler = async (
+    event: React.MouseEvent,
+    id: string,
+    service: (id: string) => Promise<string>,
+    notify: (message: string) => void,
+    details?: Book,
+) => {
+        event.stopPropagation();
+        try {
+                const res = await service(id);
+                notify(res);
+        } catch (err: any) {
+                notify(err.message);
+        }
+};
