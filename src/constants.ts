9e108f9de22f180158c9afd91c507d4917f82e74
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-17 17:41:43 +0300

    updated setBookDetails for SetStateAction

--- src/constants.ts
+++ src/constants.ts
@@ -65,17 +65,7 @@ export interface BookContextType {
     isNewBook: boolean;
     setIsNewBook: (value: boolean) => void;
     bookDetails: Book;
-    setBookDetails: (bookDetails: (prev: Book) => {
-        [p: number]: any;
-        id: string;
-        title: string;
-        author: string;
-        category: CategoryType;
-        isbn: number;
-        created_at: string | undefined;
-        modified_at: string | undefined;
-        active: boolean
-    }) => void;
+    setBookDetails: React.Dispatch<React.SetStateAction<Book | undefined>>;
     error: Error | undefined;
     setError: (err: Error) => void;
 }
