e438e6c674f526d23969b02a04eedb2bc7863633
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-22 20:33:58 +0300

    utilized SessionProvider & added route /auth

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,8 @@ import {Sidebar} from "./components/Sidebar/Sidebar.tsx";
 import {Burger} from "./components/Burger/Burger.tsx";
 import {AboutPage} from "./pages/AboutPage/AboutPage.tsx";
 import {ContactsPage} from "./pages/ContactsPage/ContactsPage.tsx";
+import {AuthPage} from "./pages/AuthPage/AuthPage.tsx";
+import {SessionProvider} from "./context/SessionContext.tsx";
 
 
 
@@ -17,6 +19,7 @@ import {ContactsPage} from "./pages/ContactsPage/ContactsPage.tsx";
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <StrictMode>
+            <SessionProvider>
                 <BookProvider>
                     <BooksProvider>
                             <Sidebar />
@@ -26,10 +29,12 @@ createRoot(document.getElementById('root')!).render(
                                 <Route path='/update-book/:id' element={<EditPage />} />
                                 <Route path='/about' element={<AboutPage />} />
                                 <Route path='/contacts' element={<ContactsPage />} />
+                                <Route path='/auth' element={<AuthPage />} />
                                 <Route path="*" element={<h1> Sorry, page Not found</h1>}/>
                             </Routes>
                     </BooksProvider>
                 </BookProvider>
+            </SessionProvider>
         </StrictMode>
     </BrowserRouter>,
 
