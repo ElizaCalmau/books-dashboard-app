bf5051f963f51caf02304837f945680f0a4339f9
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-22 12:30:40 +0300

    removed type annotation for getBookById returned value

--- src/hooks/useGetBookDetails.ts
+++ src/hooks/useGetBookDetails.ts
@@ -2,13 +2,13 @@ import {useEffect} from "react";
 
 import {getBookById} from "../utils/bookService.ts";
 import {useBookContext} from "../context/BookContext.tsx";
-import {Book, initialBook} from "../constants.ts";
+import {initialBook} from "../constants.ts";
 
 export const useGetBookDetails = (id: string) => {
     const {setIsNewBook, setBookDetails} = useBookContext()
     useEffect(() => {
             const fetchData = async () => {
-                    const data: Book | null  = await getBookById(id);
+                    const data = await getBookById(id);
                     if (!data) {
                         setIsNewBook(true);
                         setBookDetails(prev => ({
