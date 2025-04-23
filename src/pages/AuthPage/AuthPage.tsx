d28845def3ba006a364c09a1795596c35b3fb53a
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-23 19:36:46 +0300

    customized auth form and its components

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -2,14 +2,27 @@ import {supabaseClient} from "../../lib/supabaseClient.ts";
 import {ThemeSupa} from "@supabase/auth-ui-shared";
 import {Auth} from "@supabase/auth-ui-react";
 import {useAuthSubscription} from "../../hooks/useAuthSubscription.ts";
+import styles from "./AuthPage.module.scss";
+
 
 export const AuthPage = () => {
 
     const session = useAuthSubscription();
     if (!session) {
-        return (<Auth supabaseClient={supabaseClient} appearance={{ theme: ThemeSupa }} />)
+        return (
+            <div className={styles.authPageWrapper}>
+                <div className={styles.authFormWrapper}>
+                    <h2>Please log in</h2>
+                    <Auth supabaseClient={supabaseClient} appearance={{ theme:ThemeSupa ,className: {
+                            button: styles.customButton,
+                            input: styles.customInput,
+                            label: styles.customLabel
+                    },}}  providers={['google', 'github']}/>
+                </div>
+            </div>
+        )
     }
     else {
-        return (<div>Logged in!</div>)
+        return (<div className={styles.authPageWrapper}>Logged in!</div>)
     }
 };
