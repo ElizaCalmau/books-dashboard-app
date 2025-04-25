c5e9795d1b1871ebfe5c3b9cfee5592425106bd5
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-04-25 12:10:34 +0300

    created UserContext, UserProvider, and useUserContext hook

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,26 @@
+import {UserData} from "../lib/constants.ts";
+import React, {createContext, ReactNode, useContext, useState} from "react";
+
+interface UserContextType{
+    userData: UserData | null;
+    setUserData: React.Dispatch<React.SetStateAction<UserData | null>>;
+}
+
+export const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider = ({children}: {children: ReactNode}) => {
+    const [userData, setUserData] = useState<UserData | null>(null);
+    return (
+        <UserContext.Provider value={{userData, setUserData}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export const useUserContext = () => {
+    const context = useContext(UserContext);
+    if (!context) {
+        throw new Error('useUserContext must be defined');
+    }
+    return context;
+}
